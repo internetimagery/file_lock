2126bbeb78fc20db04a02f93d4077054ada99bb5
Jason <jason.dixon.email@gmail.com>
2015-08-12 00:08:39 +1200

    completed converter

--- __init__.py
+++ __init__.py
@@ -19,12 +19,12 @@ def convert(filePath):
     """
     Convert file to maya
     """
-    data = ""
+    result = []
     if os.path.isfile(filePath):
         with open(filePath, "r") as f:
             for data in f.readlines():
-                data += "\"%s\" + \n" % data.replace("\"", "\\\"").replace("\n", "\\n")
-    return data
+                result.append("\"%s\"\n" % data.replace("\"", "\\\"").replace("\n", "\\n"))
+    return "python(%s);" % " + ".join(result)
 
 root = os.path.dirname(os.path.realpath(__file__))
 script = os.path.join(root, "lock.py")
