d377a137fdd43405d65881d53de22d0dc372f6ae
Jason <jason.dixon.email@gmail.com>
2015-08-13 01:39:53 +1200

    taken a different approach using errors to drive logic, heh

--- lock.py
+++ lock.py
@@ -5,25 +5,14 @@ from datetime import datetime
 from getpass import getuser
 from json import load, dump
 
-import sys
-
-def Lock():
-    with open("%s.lock" % root, "w") as f:
-        data = {
-            "time" : str(datetime.now()),
-            "user" : getuser()
-            }
-        dump(data, f)
-    print "File locked."
-
 root = cmds.file(q=True, sn=True)
 if root:
     cmds.scriptJob(e=["quitApplication", lambda: cmds.scriptNode("File_Locker", ea=True)], kws=True)
     try:
         with open("%s.lock" % root, "r") as f:
             lockInfo = load(f)
-            if lockInfo["user"] != getuser():
-                Lock()
+            if lockInfo["user"] == getuser():
+                raise IOError, "Locked by same user"
             else:
                 message = "%(user)s locked this file at %(time)s and may be currently working on it.\nDo you wish to overide?" % lockInfo
                 answer = cmds.confirmDialog(
@@ -31,9 +20,15 @@ if root:
                     title="File is Locked",
                     message=message)
                 if "Override" in answer:
-                    Lock()
+                    raise IOError, "Overriding Lock"
                 else:
                     cmds.fileInfo("FileUID", "Not even oww")
                     cmds.file( force=True, new=True )
-    except IOError:
-        Lock()
+    except IOError, ValueError:
+        with open("%s.lock" % root, "w") as f:
+            data = {
+                "time" : str(datetime.now()),
+                "user" : getuser()
+                }
+            dump(data, f)
+        print "File locked."
