f82a32b12f7357642d7e9ffe4ae63695fc7e0bac
Jason <jason.dixon.email@gmail.com>
2015-08-12 15:33:04 +1200

    adding unique identifier to prevent referencing

--- __init__.py
+++ __init__.py
@@ -1,21 +1,23 @@
 # Lock maya file while in use
 
 import maya.cmds as cmds
-import os
+import os, time
 
 root = os.path.dirname(os.path.realpath(__file__))
 
 def injectCode():
     scriptID = "File_Locker"
-    if not cmds.objExists(scriptID) or True:
+    uid = time.time()
+    if not cmds.objExists(scriptID):
         loadCode = "print \"hello\""
-        openCode = convert(os.path.join(root, "lock.py"))
-        closeCode = convert(os.path.join(root, "unlock.py"))
+        openCode = convert(os.path.join(root, "lock.py"), uid)
+        closeCode = convert(os.path.join(root, "unlock.py"), uid)
         cmds.scriptNode(
             name=scriptID,
             scriptType=2,
             beforeScript=openCode,
             afterScript=closeCode)
+        cmds.fileInfo("FileUID", uid)
         print "Injecting Lock Code"
 
 def removeLock():
@@ -26,15 +28,20 @@ def removeLock():
         if os.path.isfile(path):
             os.remove(path)
 
-def convert(filePath):
+def convert(filePath, uid):
     """
     Convert python to mel
     """
-    result = ["\"# Line above all others\\n\""]
+    def escape(text):
+        return "\"%s\"" % text.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", "\\n").replace("\r", "")
+    result = []
+    result.append(escape("import maya.cmds as cmds\n"))
+    result.append(escape("fileID = cmds.fileInfo(\"FileUID\", q=True)\n"))
+    result.append(escape("if fileID and fileID[0] == \"%s\":\n" % uid))
     if os.path.isfile(filePath):
         with open(filePath, "r") as f:
             for data in f.readlines():
-                result.append("\"%s\"" % data.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", "\\n").replace("\r", ""))
+                result.append(escape("    " + data))
     return "python(%s);" % " + \n".join(result)
 
 import file_lock.lock
