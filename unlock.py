575f53bd263985e2adf6514967f8238f8ad5a84f
internetimagery <jason.dixon.email@gmail.com>
2015-08-28 23:55:16 +1200

    fixed lack of os import

--- unlock.py
+++ unlock.py
@@ -1,7 +1,8 @@
 # Remove the lock
 import maya.cmds as cmds
 from os.path import isfile
+from os import remove
 lockFile = "%s.lock" % cmds.file(q=True, sn=True)
 if isfile(lockFile):
-    os.remove(lockFile)
+    remove(lockFile)
     print "File unlocked."
